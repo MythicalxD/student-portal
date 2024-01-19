44f7b2c5315f472c441f0589306ccf0aa01d078d
abhi <creators462023@gmail.com>
2024-01-19 13:23:22 +0530

    fixed missing pages

---
+++ app/(routes)/dashboard/(student)/student-training/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Company } from "../student-company/components/columns";
+import { AlbumArtwork } from "../student-company/components/card-old";
+
+async function getData(token: string, session: string): Promise<Company[]> {
+    const dataToSend = {
+        id: token,
+        session: session,
+    };
+
+    const apiUrl = "/api/company";
+
+    try {
+        const response = await axios.post(apiUrl, dataToSend);
+        console.log(response);
+        return response.data;
+    } catch (error: any) {
+        window.location.href = "/login";
+        console.error();
+        return error;
+    }
+}
+
+export default function DemoPage() {
+    const [data, setData] = useState<Company[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const authToken = document.cookie
+                .split("; ")
+                .find((row) => row.startsWith("token="))
+                ?.split("=")[1];
+
+            const session = document.cookie
+                .split("; ")
+                .find((row) => row.startsWith("session="))
+                ?.split("=")[1];
+
+            const fetchedData = await getData(authToken!, session!);
+            setData(fetchedData);
+        };
+
+        fetchData(); // Call the fetchData function when the component mounts
+
+        // Optionally, you can include a cleanup function here if needed
+    }, []); // The empty dependency array ensures that the effect runs only once
+
+    return (
+        <div className="container mx-auto">
+            <div className="space-y-1">
+                <h2 className="text-2xl font-semibold tracking-tight">
+                    Training Modules
+                </h2>
+                <p className="text-sm text-muted-foreground">
+                    Explore All Modules here.
+                </p>
+            </div>
+            <Separator className="my-4" />
+            <ScrollArea>
+                <div className="flex space-x-4 pb-4">
+                    Coming soon...
+                </div>
+                <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+        </div>
+    );
+}
